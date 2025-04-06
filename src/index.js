f370969bba82f65ed171d70ae6a719f210e8f1d8
pipypupa <zong8729@gmail.com>
2025-04-06 11:11:53 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import { add, multiply } from "./src/utils/math";
+import { add, multiply } from "./utils/math";
 const result = add(5, 3) + multiply(2, 4);
 document.querySelector("#result").textContent = `Результат: ${result}`;
